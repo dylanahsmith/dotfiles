a032cf611c80715d6f826bfa418469a3cd47e5bd
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2014-05-02 20:55:35 -0400

    Avoid making rbenv a hard dependancy.

--- .bash.d/rbenv.sh
+++ .bash.d/rbenv.sh
@@ -1,2 +1,4 @@
-PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init --no-rehash -)"
+if [ -x "$HOME/.rbenv/bin/rbenv" ]; then
+  PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init --no-rehash -)"
+fi
