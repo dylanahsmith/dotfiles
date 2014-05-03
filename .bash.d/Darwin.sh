94b547c4aa8ee1b52047f8c8550d987a3b18f4b6
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2014-05-02 20:40:59 -0400

    Add open-ports alias for macs

--- .bash.d/Darwin.sh
+++ .bash.d/Darwin.sh
@@ -1,6 +1,7 @@
 alias ls='ls -G'
 export LSCOLORS="ExGxFxDxCxegedabagacad"
 alias ldd='otool -L'
+alias open-ports='sudo lsof -iTCP -sTCP:LISTEN -P'
 
 find() {
   if [ $# -eq 0 -o x"${1:0:1}" = x"-" ]; then
