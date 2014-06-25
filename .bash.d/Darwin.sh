205c8a763fdd6e02a1beed2757d7ad45efa83923
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2014-06-25 18:59:56 -0400

    alias for stat on Macs to make the output more consistent with linux.

--- .bash.d/Darwin.sh
+++ .bash.d/Darwin.sh
@@ -2,6 +2,7 @@ alias ls='ls -G'
 export LSCOLORS="ExGxFxDxCxegedabagacad"
 alias ldd='otool -L'
 alias open-ports='sudo lsof -iTCP -sTCP:LISTEN -P'
+alias stat='stat -x'
 
 find() {
   if [ $# -eq 0 -o x"${1:0:1}" = x"-" ]; then
