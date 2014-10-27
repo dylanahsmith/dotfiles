f64993b555eedf59d6d1c40649d5cfaa3096f9ec
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2014-10-27 11:44:50 -0400

    Add forward_port bash function for Mac OS X.

--- .bash.d/Darwin.sh
+++ .bash.d/Darwin.sh
@@ -11,3 +11,16 @@ find() {
     command find "$@"
   fi
 }
+
+forward_port() {
+  if [ $# -lt 3 ]; then
+    sudo ipfw list $1
+  elif [ $# -eq 3 ]; then
+    number=$1
+    source_port=$2
+    dest_port=$3
+    sudo ipfw add "$number" fwd "127.0.0.1,$dest_port" tcp from any to me dst-port "$source_port"
+  else
+    echo "usage: $0 RULE_NUM PORT"
+  fi
+}
