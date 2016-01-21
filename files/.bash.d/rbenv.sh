cd600bac22b90a80f144bfc7889604c1f228c01b
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2016-01-21 09:35:55 -0500

    rbenv: Fix bash init so it works with rbenv installed from homebrew

--- files/.bash.d/rbenv.sh
+++ files/.bash.d/rbenv.sh
@@ -1,4 +1,6 @@
-if [ -x "$HOME/.rbenv/bin/rbenv" ]; then
-  PATH="$HOME/.rbenv/bin:$PATH"
+if type rbenv >/dev/null 2>&1; then
+  if [ -x "$HOME/.rbenv/bin/rbenv" ]; then
+    PATH="$HOME/.rbenv/bin:$PATH"
+  fi
   eval "$(rbenv init --no-rehash -)"
 fi
